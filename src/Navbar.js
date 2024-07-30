0c9c3dde51b05f5df5c1b0fb97e55e4b5d5f2ee4
komeegbedi <oghenekomeegbedi222@gmail.com>
2024-07-30 01:31:41 -0500

    Added tailwind and updated navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,18 +1,16 @@
 import { Link } from "react-router-dom";
 const Navbar = () => {
-    return ( 
-       <div className="navbar">
-            <nav className="navbar-title">
-                <Link to= "/" className="navbar-logo">Route Planner</Link>
-            </nav>
-
-            <div className="navbar-links">
-                <Link to ="/Login">Login</Link>
-                <Link to ="/NewAccount">Get Started</Link>
+    return (
+        <nav className="bg-gray-800">
+            <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+                <div className="relative flex h-16 items-center justify-between">
+                    <div className="absolute inset-y-0 left-0 flex items-center">
+                        <Link to="/"> <h1 className="text-slate-300">Route Planner</h1> </Link>
+                    </div>
+                </div>
             </div>
-       </div>
-        
-     );
+        </nav>
+    );
 }
  
 export default Navbar;
