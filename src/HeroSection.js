a2b0e693126bfee6d55115d8fe3ce744d03d3b07
komeegbedi <oghenekomeegbedi222@gmail.com>
2024-07-31 21:01:16 -0500

    cleared error messages

--- src/HeroSection.js
+++ src/HeroSection.js
@@ -12,14 +12,14 @@ const HeroSection = () => {
                     <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-normal leading-normal md:text-5xl xl:text-6xl dark:text-white"> Let Us <span className='text-teal-500'>Optimize</span> Your Routes</h1>
                     <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">Less Driving = Save Gas. Save Time.</p>
                     
-                    <form class="max-w-sm mt-8">
-                    <input type="text" className="mb-5 bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:border-gray-600 placeholder-gray-700" value="Enter Starting Point (e.g. 123 Main St)"/>
-                    <input type="text" className="bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:border-gray-600 placeholder-gray-400" value="Enter Stop Address (e.g. 456 Oak Ave)" />
+                    <form className="max-w-sm mt-8">
+                    <input type="text" className="mb-5 bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:border-gray-600 placeholder-gray-600" placeholder="Enter Starting Point (e.g. 123 Main St)"/>
+                    <input type="text" className="bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:border-gray-600 placeholder-gray-600" placeholder="Enter Stop Address (e.g. 456 Oak Ave)" />
                     <button className='block mt-5 font-medium px-1 tracking-wide'><FontAwesomeIcon icon={faCirclePlus} /> Add Another Stop</button>
                     <button className ="text-white bg-gradient-to-l from-teal-400 font-medium rounded-lg text-sm px-5 py-2.5 mt-8 text-center">Optimize Route <FontAwesomeIcon icon={faLocationArrow} /> </button>
                     </form>
                 </div>
-                <div className="hidden lg:mt-0 lg:col-span-5 lg:flex rotate-12 opacity-80 faded faded-left faded-right">
+                <div className="hidden lg:mt-0 lg:col-span-5 lg:flex rotate-12 opacity-80">
                     <img src= {heroImage} alt="mockup"/>
                 </div>                
             </div>
